28ac6a0a476564dabbf7deafe4cae03c88b5e23b
JustArion <53127594+JustArion@users.noreply.github.com>
2024-11-25 12:13:29 +0100

    Add C# Lib containing BootstrapInfo struct

---
+++ src/Managed/Bootstrap.cs
@@ -0,0 +1,13 @@
+﻿namespace Dawn.AOT;
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+[StructLayout(LayoutKind.Sequential)]
+[SuppressMessage("ReSharper", "UnusedType.Global")]
+public readonly struct BootstrapInformation
+{
+    public readonly nint HINSTANCE;
+    public readonly ulong MainThreadId;
+    public readonly int MainThreadPriority;
+}
